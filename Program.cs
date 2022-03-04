e86cd55afe93210bc8b124be92c610cff3ee96a7
shayan <hakhamanesh2014@gmail.com>
2022-03-04 17:04:42 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("how much numbers do you want to enter :");
+int input =Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i < input)
+{
+    i++;
+    Console.WriteLine("please enter your numbers :");
+    var num1 = Convert.ToInt32(Console.ReadLine());
+    static void numbers(int num1)
+    {
+        int[] vs = new int[num1];
+    }
+ numbers(num1 - 1);   
+    
+    
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
